8238ed808434660445830813ee144012e400fdb7
Jiramet Berkban <jiramet.be@ku.th>
2025-07-08 11:59:42 +0700

    add new files for week01

---
+++ assignments/week 01/lab01.py/lab01.py
@@ -0,0 +1,23 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+# Write your solution here:
+full_name = input("what are you name:")
+age = input("How old are you:")
+Email = input("What is you Email:")
+Phone_number = input("What is you phone number:")
+hobby = input("What is you favorite hobby:")
+
+print("==Your profile ==")
+print("name:",full_name)
+print("Age:",age)
+print("Email:",Email)
+print("Phone Number:",Phone_number)
+print("Favorite Hobby:",hobby)

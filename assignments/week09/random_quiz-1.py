467f5d6d7cf70ae91f9fa14e9e2fc5d1adf9c8d9
Jiramet Berkban <jiramet.be@ku.th>
2025-09-23 11:50:37 +0700

    week09 6730202068

--- assignments/week09/random_quiz-1.py
+++ assignments/week09/random_quiz-1.py
@@ -25,3 +25,42 @@ Example
     Congratulations! You won in 3 attempts!
 
 """
+#แบบง่ายที่ทำเอง
+"""
+import random
+
+def test_random():
+    #สุ่มเลขระหว่าง 1-10 เก็บไว้ในตัวแปล random_number
+    random_number = random.randint(1, 7)
+    for i in range(6):
+        guessed = int(input(f"Try to guess the number (Attempt {i+1}/6): "))
+        if random_number == guessed:
+            print("You guessed correctly.")
+            break 
+        elif random_number < guessed:
+            print("Too much")
+        elif random_number > guessed:
+            print("Too low")
+        print(random_number) 
+    
+test_random()
+"""
+#ตามอาจาร์
+import random
+def test_random():
+    print("=== SIMPLE GUESSING GAME ===")
+    print("Guess my number between 1 and 20!")
+    print("You habe 6 attempts.")
+
+    random_number = random.randint(1,10)
+    for i in range(6):
+        guessed = int(input(f"Try to guess the number (Attempt {i+1}/6): "))
+        if random_number == guessed:
+            print(f"You guessed correctly.{i+1} attempt")
+            break 
+        elif random_number < guessed:
+            print("Too much")
+        elif random_number > guessed:
+            print("Too low")
+        print(random_number) 
+test_random()
--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,16 @@
 import random
 
 def test_random():
+    #สุ่มเลขระหว่าง 1-10 เก็บไว้ในตัวแปล random_number
     random_number = random.randint(1, 100)
-    print(random_number)
+    guessed = int(input("Try to guess the number.:"))
+    if random_number == guessed:
+        print("You guessed correctly.")
+    if random_number < guessed:
+        print("Too much")
+    if random_number > guessed:
+        print("Too low")
+    print(random_number) 
     
+
 test_random()

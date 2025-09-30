0c9444508fa7f614fde858160ea3911fe0be1f58
Jiramet Berkban <jiramet.be@ku.th>
2025-09-30 12:08:27 +0700

    lab_week10

--- assignments/week10/personal-validator.py
+++ assignments/week10/personal-validator.py
@@ -28,3 +28,58 @@ Age Group: Young Adult (18-30)
 Phone: +91-9876543210
 
 """
+
+print("=== PERSONAL INFORMATION VALIDATOR ===")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+phone_number = input("Enter your phone: ")
+
+# Name validation
+nameFlag = True
+for char in name:
+    if not (char.isalpha() or char == " "):
+        nameFlag = False
+        break
+
+# Age validation
+ageFlag = True
+try:
+    age_int = int(age)
+    if age_int < 18 or age_int > 65:
+        ageFlag = False
+except ValueError:
+    ageFlag = False
+
+# Phone validation
+phoneFlag = True
+if len(phone_number) != 10 or not phone_number.isdigit():
+    phoneFlag = False
+
+# Display results
+print("\nValidation Results:")
+if nameFlag:
+    print("Name: Valid (contains only letters and spaces)")
+else:
+    print("Name: Invalid (contains non-letter characters)")
+
+if ageFlag:
+    print(f"Age: Valid ({age} years old)")
+else:
+    print("Age: Invalid (not a number, or not between 18 and 65)")
+
+if phoneFlag:
+    print("Phone: Valid (10-digit number)")
+else:
+    print("Phone: Invalid (not 10-digit number)")
+
+# Formatted output
+print("\nFormatted Information")
+print("Name:", name.upper(), name.lower(), name.capitalize())
+
+if ageFlag and 18 <= age_int <= 30:
+    print("Age Group: Young Adult (18-30)")
+elif ageFlag:
+    print("Age Group: Not Young Adult")
+
+if phoneFlag:
+    print("Phone: +66-%s" % phone_number)
--- assignments/week10/string-manipulate.py
+++ assignments/week10/string-manipulate.py
@@ -47,3 +47,22 @@ Transformations:
 USE: len(), split(), count(), upper(), lower(), title(), slicing operations
 
 """
+
+string = "The Quick Brown Fox Jumps Over The Lazy Dog"
+
+print("=== TEXT ANALYSIS REPORT ===")
+print("Character Analysis:")
+print("- Total characters: %d (with spaces), %d (without spaces)" % (len(string), len(string) - string.count(" ")))
+strlower = string.lower()
+vowels = string.count("a") + strlower.count("e") + strlower.count("i") + strlower.count("o") + strlower.count("u")
+vowelsStr = ""
+for char in strlower:
+    if char in ['a','e','i','o','u']:
+        vowelsStr += char +","
+
+print("-Vowels: %d (%s)" % (vowels, vowelsStr))
+print("- Consonants: %d" %(len(string)- int(string.count(" ") -vowels)))
+
+words = string.split()
+print("Word Analysis:")
+print("- Total words: ", len(words))

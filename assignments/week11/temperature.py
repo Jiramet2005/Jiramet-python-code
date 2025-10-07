c88f597a1646a0a557e2f728c2f46eb5d6e55376
Jiramet Berkban <jiramet.be@ku.th>
2025-10-07 11:43:27 +0700

    week11 lab

--- assignments/week11/temperature.py
+++ assignments/week11/temperature.py
@@ -23,3 +23,27 @@ Lowest: 19 C
 
 """
     
+
+def get_temperatures():
+    x = [32, 30, 31, 33, 30, 31, 34]
+    return x
+
+def analyze(temp_list):
+    sum = 0
+    for temp in temp_list:
+        sum += temp
+    average = sum / len(temp_list)
+    max_temp = max(temp_list)
+    min_temp = min(temp_list)
+    return (average, max_temp, min_temp)
+
+def display_analysis(avg, high, low):
+    print("Temperature Analysis for the week:")
+    print("Average: " + str(avg) + " C")
+    print(f"Highest: {high} C")
+    print(f"Lowest: {low} C")
+
+temp_list = get_temperatures()
+result = analyze(temp_list)
+display_analysis(result[0], result[1], result[2])
+
